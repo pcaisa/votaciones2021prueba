7c40f66024c0b9ccd92c399c5d7b6b697848b17b
Distribuidos <75593762+pcaisa@users.noreply.github.com>
2021-06-25 16:27:13 -0500

    3
    x<x

--- index.php
+++ index.php
@@ -1,86 +1,86 @@
-
-<!DOCTYPE html>
-<html lang="es">
-
-<head>
-  <title>Dinamicos</title>
-  <link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
-  <link href="http://cdnjs.cloudflare.com/ajax/libs/jQuery-Validation-Engine/2.6.4/validationEngine.jquery.css" rel="stylesheet">
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/jQuery-Validation-Engine/2.6.4/jquery.validationEngine.min.js"></script>
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/jQuery-Validation-Engine/2.6.4/languages/jquery.validationEngine-es.js"></script>
-  <style>
-    .top-buffer {
-      margin-top: 20px;
-    }
-  </style>
-</head>
-
-<body>
-  <div id="container ">
-    <div class="row-fluid top-buffer">
-      <div class="col-lg-6 col-lg-offset-3 text-center">
-        <form id="miform" method="post" name="miform" action="guarda.php">
-		
-
-
-
-   
-
-   
-          <table id="tblprod" class="table table-hover table-bordered">
-            <thead>
-              <tr>
-                <th>id</th>
-                <th>Nombre</th>
-				<th>eliminar</th>
-				
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>
-                  <div class="form-group col-lg-4">
-                    <input class="form-control validate[required]" name="nombre[]" />
-					</div>
-					<div class="form-group col-lg-4">
-					<input class="form-control validate[required]" type="number" name="limonesusados[]" min="1" max="5"/>
-  
-                  </div>
-                </td>
-				<td>
-		<div class="form-group col-lg-4">
-				<a href="index.html?id=<?php echo $row['cod_estudiante'] ?>" class="btn btn-danger">Eliminar</a>
-				</div>
-				</td>
-              </tr>
-            </tbody>
-          </table>
-          <button id="btnadd" class="btn btn-primary">Agregar Nuevo</button>
-		  
-        </form>
-      </div>
-    </div>
-  </div>
-
-  <script type="text/javascript">
-    $(function() {
-      var count = 1;
-      jQuery("#miform").validationEngine({
-        promptPosition: "centerRight:0,-5"
-      });
-
-      $(document).on("click", "#btnadd", function(event) {
-        count++;
-        $('#tblprod tr:last').after('<tr><td>' + count + '</td><td><div class="form-group col-lg-4"><input class="form-control validate[required]" name=" nombre[]" /></div><div class="form-group col-lg-4"><input class="form-control validate[required]" type="number" name="limonesusados[]" min="1" max="5"/> </div></td></tr>');
-        event.preventDefault();
-
-      });
-
-    });
-  </script>
-
-</body>
-
-</html>
+<?php
+echo "<!DOCTYPE html>";
+echo "<html lang=\"es\">";
+echo "";
+echo "<head>";
+echo "<title>Dinamicos</title>";
+echo "<link href=\"http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css\" rel=\"stylesheet\">";
+echo "<link href=\"http://cdnjs.cloudflare.com/ajax/libs/jQuery-Validation-Engine/2.6.4/validationEngine.jquery.css\" rel=\"stylesheet\">";
+echo "<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js\"></script>";
+echo "<script src=\"http://cdnjs.cloudflare.com/ajax/libs/jQuery-Validation-Engine/2.6.4/jquery.validationEngine.min.js\"></script>";
+echo "<script src=\"http://cdnjs.cloudflare.com/ajax/libs/jQuery-Validation-Engine/2.6.4/languages/jquery.validationEngine-es.js\"></script>";
+echo "<style>";
+echo ".top-buffer {";
+echo "margin-top: 20px;";
+echo "}";
+echo "</style>";
+echo "</head>";
+echo "";
+echo "<body>";
+echo "<div id=\"container \">";
+echo "<div class=\"row-fluid top-buffer\">";
+echo "<div class=\"col-lg-6 col-lg-offset-3 text-center\">";
+echo "<form id=\"miform\" method=\"post\" name=\"miform\" action=\"guarda.php\">";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "<table id=\"tblprod\" class=\"table table-hover table-bordered\">";
+echo "<thead>";
+echo "<tr>";
+echo "<th>id</th>";
+echo "<th>Nombre</th>";
+echo "<th>eliminar</th>";
+echo "";
+echo "</tr>";
+echo "</thead>";
+echo "<tbody>";
+echo "<tr>";
+echo "<td>1</td>";
+echo "<td>";
+echo "<div class=\"form-group col-lg-4\">";
+echo "<input class=\"form-control validate[required]\" name=\"nombre[]\" />";
+echo "</div>";
+echo "<div class=\"form-group col-lg-4\">";
+echo "<input class=\"form-control validate[required]\" type=\"number\" name=\"limonesusados[]\" min=\"1\" max=\"5\"/>";
+echo "";
+echo "</div>";
+echo "</td>";
+echo "<td>";
+echo "<div class=\"form-group col-lg-4\">";
+echo "<a href=\"index.html?id=<?php echo $row['cod_estudiante'] ?>\" class=\"btn btn-danger\">Eliminar</a>";
+echo "</div>";
+echo "</td>";
+echo "</tr>";
+echo "</tbody>";
+echo "</table>";
+echo "<button id=\"btnadd\" class=\"btn btn-primary\">Agregar Nuevo</button>";
+echo "";
+echo "</form>";
+echo "</div>";
+echo "</div>";
+echo "</div>";
+echo "";
+echo "<script type=\"text/javascript\">";
+echo "$(function() {";
+echo "var count = 1;";
+echo "jQuery(\"#miform\").validationEngine({";
+echo "promptPosition: \"centerRight:0,-5\"";
+echo "});";
+echo "";
+echo "$(document).on(\"click\", \"#btnadd\", function(event) {";
+echo "count++;";
+echo "$('#tblprod tr:last').after('<tr><td>' + count + '</td><td><div class=\"form-group col-lg-4\"><input class=\"form-control validate[required]\" name=\" nombre[]\" /></div><div class=\"form-group col-lg-4\"><input class=\"form-control validate[required]\" type=\"number\" name=\"limonesusados[]\" min=\"1\" max=\"5\"/> </div></td></tr>');";
+echo "event.preventDefault();";
+echo "";
+echo "});";
+echo "";
+echo "});";
+echo "</script>";
+echo "";
+echo "</body>";
+echo "";
+echo "</html>";
